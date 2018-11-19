f64a384678b8d2b984940fea4f763e925b0d883a
Viterlen <tomasz670zielinski@gmail.com>
2018-11-19 19:49:28 +0100

    nav

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+let nav = '<div>\n' +
+    '\n' +
+    '<ul>\n' +
+    '    <li><a href="index.html">Głowna<a/></li>\n' +
+    '    <li><a href="mapa.html">Mapa<a/></li>\n' +
+    '    <li><a href="contact.html">Formularz<a/></li>\n' +
+    '</ul>\n' +
+    '\n' +
+    '</div>' ;
+
+
+
+let navbar = document.querySelector('.navbar');
+
+navbar.innerHTML = nav;
+
+
+
+
+
